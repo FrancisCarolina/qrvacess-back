cc37ac740ff2fdfefd0ecc89ce4ee1f2e54ee670
Francis Carolina <francis.carolinalovo@gmail.com>
2024-11-22 21:20:59 -0300

    tirando middleware do validar codigo

--- routes/codigoRoutes.js
+++ routes/codigoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const codigoController = require("../controllers/codigoController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post("/codigo/validar",authMiddleware.verifyToken, codigoController.validarCodigo);
+router.post("/codigo/validar", codigoController.validarCodigo);
 router.post('/condutor/:id/codigo',authMiddleware.verifyToken, codigoController.gerarCodigo);
 
 module.exports = router;
