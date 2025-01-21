a84e3630c23c787fbe1adfcc0b22f7fbc73763f8
Francis Carolina <francis.carolinalovo@gmail.com>
2025-01-21 16:24:50 -0300

    colocando filtro de data no relatorio

--- controllers/historicoController.js
+++ controllers/historicoController.js
@@ -58,7 +58,7 @@ exports.getHistoricoPorLocal = async (req, res) => {
           where: { id },
           include: {
             model: Usuario,
-            attributes: [],
+            attributes: ["id"],
             include: {
               model: Condutor,
               attributes: ['nome', 'cpf', 'ativo'],
